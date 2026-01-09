7d9c68ebb66f770ecb06c91c901041588b470a7a
Roman Mokshin <drobitek76106055@gmail.com>
2026-01-09 09:12:42 +0300

    feat: add text style schemes

--- lib/src/core/ui_kit/theme/theme_x.dart
+++ lib/src/core/ui_kit/theme/theme_x.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tails_mobile/src/core/ui_kit/colors/ui_color_scheme.dart';
+import 'package:tails_mobile/src/core/ui_kit/typos/text_style_tokens.dart';
+import 'package:tails_mobile/src/core/ui_kit/typos/ui_text_scheme.dart';
 
 extension ThemeX on BuildContext {
+  UiTextStyle get uiFonts => uiTextScheme.tokens;
+
   UiColorScheme get uiColors => uiColorScheme;
 }
---
+++ lib/src/core/ui_kit/typos/text_style_tokens.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+// TODO: Change to generated constant from flutter_gen after adding fonts to assets
+const String _gilroyFont = 'Gilroy';
+const String _manropeFont = 'Manrope';
+
+abstract class UiTextStyle {
+  TextStyle get header80Regular;
+
+  TextStyle get header76Help;
+
+  TextStyle get header62Medium;
+
+  TextStyle get header62Regular;
+
+  TextStyle get header52Regular;
+
+  TextStyle get header42Regular;
+
+  TextStyle get header36Semibold;
+
+  TextStyle get header32Semibold;
+
+  TextStyle get header32Regular;
+
+  TextStyle get header28Semibold;
+
+  TextStyle get header24Semibold;
+
+  TextStyle get header20Medium;
+
+  TextStyle get text20Semibold;
+
+  TextStyle get text16Regular;
+
+  TextStyle get text16Medium;
+
+  TextStyle get text16Semibold;
+
+  TextStyle get text14Regular;
+
+  TextStyle get text14Medium;
+
+  TextStyle get text14Semibold;
+
+  TextStyle get text12Regular;
+
+  TextStyle get text12Medium;
+
+  TextStyle get text12Semibold;
+}
+
+class UiDefaultTextStyleTokens extends UiTextStyle {
+  @override
+  TextStyle get header80Regular => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 80.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.8,
+  );
+
+  @override
+  TextStyle get header76Help => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 76.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.76,
+  );
+
+  @override
+  TextStyle get header62Medium => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 62.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.62,
+  );
+
+  @override
+  TextStyle get header62Regular => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 62.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -1.24,
+  );
+
+  @override
+  TextStyle get header52Regular => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 52.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.52,
+  );
+
+  @override
+  TextStyle get header42Regular => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 42.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.84,
+  );
+
+  @override
+  TextStyle get header36Semibold => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 36.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.72,
+  );
+
+  @override
+  TextStyle get header32Semibold => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 32.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.32,
+  );
+
+  @override
+  TextStyle get header32Regular => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 32.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.32,
+  );
+
+  @override
+  TextStyle get header28Semibold => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 28.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.56,
+  );
+
+  @override
+  TextStyle get header24Semibold => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get header20Medium => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.2,
+  );
+
+  @override
+  TextStyle get text20Semibold => const TextStyle(
+    fontFamily: _gilroyFont,
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.2,
+  );
+
+  @override
+  TextStyle get text16Regular => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.16,
+  );
+
+  @override
+  TextStyle get text16Medium => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.16,
+  );
+
+  @override
+  TextStyle get text16Semibold => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text14Regular => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text14Medium => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text14Semibold => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text12Regular => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text12Medium => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 12.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.0,
+  );
+
+  @override
+  TextStyle get text12Semibold => const TextStyle(
+    fontFamily: _manropeFont,
+    fontSize: 12.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.0,
+  );
+}
