1aaf321d852801f0a27353885c2ff297097198d9
Roman Mokshin <drobitek76106055@gmail.com>
2026-01-09 08:40:14 +0300

    feat: setup color scheme

---
+++ lib/src/core/ui_kit/theme/theme_x.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tails_mobile/src/core/ui_kit/colors/ui_color_scheme.dart';
+
+extension ThemeX on BuildContext {
+  UiColorScheme get uiColors => uiColorScheme;
+}
