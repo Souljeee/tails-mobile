b7796dccb487320a8c4ce8684d88956f07a3a62f
Roman Mokshin <drobitek76106055@gmail.com>
2026-01-26 08:02:37 +0300

    feat: add pets screen base design

--- lib/src/feature/pets/pets_list/domain/pets_list_bloc.dart
+++ lib/src/feature/pets/pets_list/domain/pets_list_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:tails_mobile/src/feature/pets/core/data/repositories/models/pet_model.dart';
 import 'package:tails_mobile/src/feature/pets/core/data/repositories/pet_repository.dart';
+import 'package:tails_mobile/src/feature/pets/core/enums/pet_type_enum.dart';
 
 part 'pets_list_event.dart';
 part 'pets_list_state.dart';
@@ -13,7 +14,8 @@ class PetsListBloc extends Bloc<PetsListEvent, PetsListState> {
 
   PetsListBloc({
     required PetRepository petRepository,
-  }) : _petRepository = petRepository, super(const PetsListState.loading()) {
+  })  : _petRepository = petRepository,
+        super(const PetsListState.loading()) {
     on<PetsListEvent>(
       (event, emit) => event.map(
         fetchRequested: (event) => _onFetchRequested(event, emit),
@@ -28,9 +30,13 @@ class PetsListBloc extends Bloc<PetsListEvent, PetsListState> {
     try {
       emit(const PetsListState.loading());
 
-      final pets = await _petRepository.getPets();
+      //final pets = await _petRepository.getPets();
 
-      emit(PetsListState.success(pets: pets));
+      Future.delayed(const Duration(seconds: 2), () {
+        emit(PetsListState.success(pets: _mockedPets));
+      });
+
+      //emit(PetsListState.success(pets: pets));
     } catch (e, s) {
       addError(e, s);
 
@@ -38,3 +44,42 @@ class PetsListBloc extends Bloc<PetsListEvent, PetsListState> {
     }
   }
 }
+
+final _mockedPets = [
+  PetModel(
+    id: '1',
+    name: 'Rex',
+    image: 'https://petstory.ru/knowledge/dogs/dog-adoption/samye-krasivye-porody-sobak-v-mire/',
+    petType: PetTypeEnum.dog,
+    birthday: DateTime.now().subtract(Duration(days: 365)),
+    breed: 'Labrador',
+    gender: 'Male',
+    color: 'Black',
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+  ),
+  PetModel(
+    id: '2',
+    name: 'Bella',
+    image: 'https://vethelp72.ru/stati/mkb-u-kotov',
+    petType: PetTypeEnum.cat,
+    birthday: DateTime.now().subtract(Duration(days: 1000)),
+    breed: 'Siamese',
+    gender: 'Female',
+    color: 'White',
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+  ),
+  PetModel(
+    id: '3',
+    name: 'Charlie',
+    image: 'https://domkohouse.com.ua/ru/yak-viduchyty-kota-mityty-terytoriiu/',
+    petType: PetTypeEnum.cat,
+    birthday: DateTime.now().subtract(Duration(days: 1500)),
+    breed: 'Siamese',
+    gender: 'Male',
+    color: 'White',
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+  ),
+];
--- lib/src/feature/pets/pets_list/presentation/pets_screen.dart
+++ lib/src/feature/pets/pets_list/presentation/pets_screen.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tails_mobile/src/core/ui_kit/components/ui_app_bar/ui_app_bar.dart';
 import 'package:tails_mobile/src/core/ui_kit/theme/theme_x.dart';
+import 'package:tails_mobile/src/feature/initialization/widget/dependencies_scope.dart';
+import 'package:tails_mobile/src/feature/pets/core/data/repositories/models/pet_model.dart';
+import 'package:tails_mobile/src/feature/pets/pets_list/domain/pets_list_bloc.dart';
 
-class PetsScreen extends StatelessWidget {
+class PetsScreen extends StatefulWidget {
   const PetsScreen({super.key});
+
+  @override
+  State<PetsScreen> createState() => _PetsScreenState();
+}
+
+class _PetsScreenState extends State<PetsScreen> {
+  late final PetsListBloc _petsListBloc =
+      PetsListBloc(petRepository: DependenciesScope.of(context).petRepository);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _petsListBloc.add(const PetsListEvent.fetchRequested());
+  }
+
+  @override
+  void dispose() {
+    _petsListBloc.close();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +43,105 @@ class PetsScreen extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: context.uiColors.orangePrimary,
+        child: const Icon(Icons.add),
+      ),
       body: SafeArea(
-        child: Column(
+        child: BlocBuilder<PetsListBloc, PetsListState>(
+          bloc: _petsListBloc,
+          builder: (context, state) {
+            return state.map(
+              loading: (_) => const SizedBox.shrink(),
+              success: (state) => _PetsList(pets: state.pets),
+              error: (_) => const SizedBox.shrink(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _PetsList extends StatelessWidget {
+  final List<PetModel> pets;
+
+  const _PetsList({required this.pets});
+
+  @override
+  Widget build(BuildContext context) {
+    return pets.isEmpty
+        ? const _PetsEmptyList()
+        : ListView.builder(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 24,
+            ),
+            itemCount: pets.length,
+            itemBuilder: (context, index) {
+              return _PetItem(pet: pets[index]);
+            },
+          );
+  }
+}
+
+class _PetItem extends StatelessWidget {
+  final PetModel pet;
+
+  const _PetItem({required this.pet});
+
+  String get petAge {
+    final age = DateTime.now().difference(pet.birthday).inDays;
+
+    final years = age ~/ 365;
+    final months = (age % 365) ~/ 30;
+
+    return years > 0 ? '$years лет $months месяцев' : '$months месяцев';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: context.uiColors.white,
+        borderRadius: BorderRadius.circular(32),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
           children: [
-            Text('Мои питомцы'),
+            Image.network(pet.image),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          pet.name,
+                          style: context.uiFonts.header24Semibold,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.arrow_forward_ios),
+                        iconSize: 16,
+                        color: context.uiColors.black100.withValues(alpha: 0.5),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '${pet.petType.name} · $petAge',
+                    style: context.uiFonts.text16Medium.copyWith(color: context.uiColors.brown),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -28,8 +149,20 @@ class PetsScreen extends StatelessWidget {
   }
 }
 
+class _PetsEmptyList extends StatelessWidget {
+  const _PetsEmptyList();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Список ваших питомцев пуст'),
+    );
+  }
+}
+
 class _NotificationsButton extends StatelessWidget {
   final VoidCallback onPressed;
+
   const _NotificationsButton({required this.onPressed});
 
   @override
